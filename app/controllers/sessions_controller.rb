b623203e5ad6959322bdc4fcca6560530df27d28
Rashad-Muntar <muntartoure@gmail.com>
2021-05-03 09:59:14 +0000

    add user authenction for sign up and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
-class UsersController < ApplicationController
+class SessionsController < ApplicationController
+
+    def new
+        
+    end
+
+    def create
+        user = User.find_by(name: params[:session][:name].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash = "You have succesffuly logged out"
+        redirect_to root_path
+    end
+    
+
 end
