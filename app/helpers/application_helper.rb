4ad35e05d5420e1507f1a527d4726486b584eeaa
Rashad-Muntar <muntartoure@gmail.com>
2021-05-03 10:14:50 +0000

    add user authetication logic to helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+    def show_sign_in
+        out = ''
+        out << link_to(link_to('Sign In', login_path)) unless logged_in?
+        out.html_safe
+      end
+
+      def show_sign_up
+        out = ''
+        out << link_to(link_to('Sign up', signup_path)) unless logged_in?
+        out.html_safe
+      end
+
+      def show_sign_out
+        out = ''
+        out << link_to(link_to('Sign out', signout_path, method: :delete)) if logged_in?
+        out.html_safe
+      end
 end
